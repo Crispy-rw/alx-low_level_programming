c1f6e9d8e80f457f1e0b2c4615ad09b435c5e737
Christian <nshimyumukizachristian@gmail.com>
2021-02-03 20:29:00 +0000

    Fix 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+*  main - print to the standard output 'Programming is multilingual puzzle'
+*
+*  Use Puts
+*  Return: retrn 0
+*/
+
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
