78c22d01cbb35db5b14d17c8566c68c2b060a9e0
Christian <nshimyumukizachristian@gmail.com>
2021-02-04 19:48:10 +0000

    Fix

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,8 +7,8 @@
  *	 followed by a new line.
  *
  * You are not allowed to use any variable of type char
- * You can only use the putchar function (every other function 
- 	 (printf, puts, etc…) is forbidden)
+ * You can only use the putchar function (every other function
+ *	(printf, puts, etc…) is forbidden)
  * You can only use putchar twice in your code
  * All your code should be in the main function
  * Return: 0
