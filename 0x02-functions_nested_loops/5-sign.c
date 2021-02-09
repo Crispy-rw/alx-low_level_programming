50c1f3f5660106c28c3434646c872867c72d6a4e
Christian <nshimyumukizachristian@gmail.com>
2021-02-09 19:24:03 +0000

    Add Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+* print_sign - prints the sign of a number.
+*
+*@n: function argument
+*
+*Return: Returns 1 if n is greater than zero or return 0 if n is zero 
+*and return -1 if n is less than zero
+*/
+
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+
+return (0);
+}
