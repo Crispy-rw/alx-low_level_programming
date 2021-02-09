94e16f10895b074c33d66d2a55016629ee8c4194
Christian <nshimyumukizachristian@gmail.com>
2021-02-09 20:07:11 +0000

    Add Two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+
+/**
+* _add -  adds two integers and returns the result.
+*
+*@number1: first argument
+*@number2: second argument
+*
+*Return: return value after adding two number
+*/
+
+
+int _add(int number1, int number2)
+{
+	int sum = number1 + number2;
+
+	return (sum);
+}
