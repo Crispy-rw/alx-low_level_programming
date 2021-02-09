a65c899c8fbd8af26848d8cb58ba7cec8dce8357
Christian <nshimyumukizachristian@gmail.com>
2021-02-09 20:38:21 +0000

    Add questio 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute of the day of jack bauer
+ * 
+ * Return: return void
+ */
+void jack_bauer(void)
+{
+int a, b;
+
+for (a = 0; a < 24; a++)
+{
+for (b = 0; b < 60; b++)
+{
+_putchar('0' + (h / 10));
+_putchar('0' + (h % 10));
+_putchar(':');
+_putchar('0' + (m / 10));
+_putchar('0' + (m % 10));
+_putchar('\n');
+}
+}
+}
