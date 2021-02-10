e7e7ee2302e7e11577879767ffd1f0757775168f
Christian <nshimyumukizachristian@gmail.com>
2021-02-10 20:34:08 +0000

    Fix 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,7 +13,7 @@ void print_alphabet_x10(void)
 char alpha;
 int counter;
 
-for (counter = 0; counter <= 10; counter++)
+for (counter = 1; counter <= 10; counter++)
 {
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
