3186b4cbf0fe034d1ee81cbc8973f6f4095174d4
Christian <nshimyumukizachristian@gmail.com>
2021-02-10 20:51:02 +0000

    Add Fibo

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 98 Fibonacci numbers, starting with 1 and 2,
+ *  followed by a new line.
+ *
+ * Return: On success 1.
+ */
+
+int main(void)
+{
+	int i;
+	float a, b, r;
+
+	a = 1;
+	b = 2;
+	r = 0;
+
+	printf("1, 2");
+
+	for (i = 1; i <= 96; i++)
+	{
+		r = a + b;
+		a = b;
+		b = r;
+		printf(", %.0f", r);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
