c6db55db546e4469495dab6eb262f55962b4c831
Christian <nshimyumukizachristian@gmail.com>
2021-02-19 16:01:56 +0000

    Add Task cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**  islower -checker where a character is in lowecase
+*
+*@c: character to be checked
+*Return: 1 if c is lowercase esle 0
+*/
+
+
+
+char *cap_string(char *s)
+{
+int i;
+
+i = 0;
+
+while (*(s + i) != '\0')
+{
+if (*(s + 0) >= 'a' && *(s + 0) <= 'z')
+{
+*(s + 0) = *(s + 0) - 32;
+}
+if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
+s[i] == ',' || s[i] == ';' || s[i] == '.' ||
+s[i] == '!' || s[i] == '?' || s[i] == '\"' ||
+s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+{
+if (*(s + i + 1) >= 'a' && *(s + i + 1) <= 'z')
+*(s + i + 1) = *(s + i + 1) - 32;
+}
+i++;
+}
+return (s);
+}
