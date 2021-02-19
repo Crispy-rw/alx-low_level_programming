a9f4090cd5176b7d061e6d7a3a89dfcf2db77f83
Christian <nshimyumukizachristian@gmail.com>
2021-02-19 10:25:13 +0000

    Add Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+
+/**
+* _strlen - return the length of a string
+*
+*@c: string to be counted
+*Return: Integer
+*/
+
+
+int _strlen(char *c)
+{
+
+int i;
+while (*(c + i) != '\0')
+{
+	i++;
+}
+return (i);
+}
+
+/**
+* _strcat - return the concatenates two strings
+*
+*@dest: destinations string
+*@src: source string
+* Return: destinations string
+*/
+
+
+char *_strcat(char *dest, char *src)
+{
+
+int i = 0;
+int len = _strlen(dest);
+while (*(src + i) != '\0')
+{
+	*(dest + len) = *(src + i);
+	len++;
+	i++;
+}
+return (dest);
+}
