28d2fd948e1c2a6d59a701a2aa6f63e16d1f3604
Christian <nshimyumukizachristian@gmail.com>
2021-03-10 20:57:32 +0000

    Fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -11,7 +11,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-
+if (name != 0 && f != 0)
 f(name);
 
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,10 +10,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-size_t count;
+unsigned int count;
+if (array != 0 && size >= 1 && action != 0)
+{
 for (count = 0; count < size; count++)
 {
 action(array[count]);
+}
 }
 }
 
