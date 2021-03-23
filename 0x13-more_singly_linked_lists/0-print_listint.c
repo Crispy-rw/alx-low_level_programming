9942aca15630b7adea4dd203db06f3a30bed7d79
Christian <nshimyumukizachristian@gmail.com>
2021-03-23 20:09:22 +0000

    fix 1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,13 @@
 size_t print_listint(const listint_t *h)
 {
 
-unsigned int count = 0;
+int count = 0;
 const listint_t *current = h;
 
 while (current != NULL)
 {
 
-	printf("%d \n", current->n);
+	printf("%d\n", current->n);
 	current = current->next;
 	count++;
 }
