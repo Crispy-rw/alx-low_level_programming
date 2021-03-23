28c4e3e0d950b92abf719081d5bbe063f5603ddb
Christian <nshimyumukizachristian@gmail.com>
2021-03-23 21:56:52 +0000

    fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,10 +12,10 @@ void free_listint2(listint_t **head)
 
 listint_t *list;
 
-if (head != NULL)
+if (head == NULL)
 return;
 
-while (*head)
+while (head != NULL)
 {
 list = *head;
 *head = list->next;
