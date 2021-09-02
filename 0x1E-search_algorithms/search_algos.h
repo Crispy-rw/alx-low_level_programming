f721dc7b168721423c54a37f1a685ea9fff70dfc
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-09-02 08:41:11 +0000

    feat: linear search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,7 @@
+#ifndef SEARCH_ALG
+#define SEARCH_ALG
+#include <stdio.h>
+#include <stdlib.h>
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
